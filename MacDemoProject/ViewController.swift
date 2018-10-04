f8abd858785a93eb0240556664c0521186ce523e
Matt Moriarity <matt@mattmoriarity.com>
2018-10-04 07:21:27 -0500

    Initial Commit

---
+++ MacDemoProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MacDemoProject
+//
+//  Created by Matt Moriarity on 10/4/18.
+//  Copyright © 2018 Travis CI Gmbh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
